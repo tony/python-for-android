583e7f2a7775e2d8f8d7b9aece1344152bd058b7
Tony Narlock <tony@git-pull.com>
2015-07-31 22:00:51 -0500

    DEBUG

--- recipes/pillow/recipe.sh
+++ recipes/pillow/recipe.sh
@@ -2,12 +2,29 @@
 
 VERSION_pillow=${VERSION_pillow:-2.9.0}
 URL_pillow=https://pypi.python.org/packages/source/P/Pillow/Pillow-$VERSION_pillow.tar.gz
-DEPS_pillow=(png jpeg python)
+DEPS_pillow=(png jpeg python setuptools)
 MD5_pillow=46f1729ece27981d54ec543ad5b37d14
 BUILD_pillow=$BUILD_PATH/pillow/$(get_directory $URL_pillow)
 RECIPE_pillow=$RECIPES_PATH/pillow
 
+DEBUG_pillow="DEBUG"
+if [ -z "$DEBUG_pillow" ]; then
+    DEBUG_repo=$HOME/study/python/pillow
+fi
+
+if [ -n "$DEBUG_pillow" ]; then
+    echo "debug"
+    BUILD_pillow=$DEBUG_repo
+    echo $DEBUG_repo
+fi
+
 function prebuild_pillow() {
+        if [ -n "$DEBUG_pillow" ]; then
+            # no patching on DEBUG, since we're targetting the branch
+            # we use to generate patches from.
+            return
+        fi
+
 	cd $BUILD_pillow
 
 	# check marker in our source build
@@ -17,7 +34,7 @@ function prebuild_pillow() {
 	fi
 
 	try cp setup.py setup.py.tmpl
-	try patch -p1 < $RECIPE_pillow/patches/fix-path.patch
+	# try patch -p1 < $RECIPE_pillow/patches/fix-path.patch
 
 	LIBS="$SRC_PATH/obj/local/$ARCH"
 	try cp setup.py.tmpl setup.py
@@ -25,7 +42,7 @@ function prebuild_pillow() {
 	try $SED s:_JNI_:$JNI_PATH: setup.py
 	try $SED s:_NDKPLATFORM_:$NDKPLATFORM: setup.py
 
-	try patch -p1 < $RECIPE_pillow/patches/disable-tk.patch
+	# try patch -p1 < $RECIPE_pillow/patches/disable-tk.patch
 
 	# everything done, touch the marker !
 	touch .patched
@@ -39,10 +56,14 @@ function shouldbuild_pillow() {
 
 function build_pillow() {
 	cd $BUILD_pillow
-
-	push_arm
-        echo $SRC_PATH/obj/local/$ARCH
+        echo "DEBUG_pillow $DEBUG_pillow"
+        echo "DEBUG_repo $DEBUG_repo"
+        echo "pwd $PWD"
+        echo "build_pillow dir: $BUILD_pillow"
+        echo "obj dir: $SRC_PATH/obj/local/$ARCH"
         ls $SRC_PATH/obj/local/$ARCH
+	push_arm
+
 	LIBS="$SRC_PATH/obj/local/$ARCH"
 	export CFLAGS="$CFLAGS -I$JNI_PATH/png -I$JNI_PATH/jpeg -I$JNI_PATH/freetype/include/freetype"
 	export LDFLAGS="$LDFLAGS -L$LIBS -lm -lz"
